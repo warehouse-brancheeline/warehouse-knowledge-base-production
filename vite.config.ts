3017f64ea4bd7b1df63a041a3163e4002b5c3f6d
warehouse-brancheeline <headwarehouse.brancheeline@gmail.com>
2026-07-25 20:44:46 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/warehouse-knowledge-base/',
+  base: '/warehouse-knowledge-base-production/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
